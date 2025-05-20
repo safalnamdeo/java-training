1c632ba1001dd713ac3344011e5491624d1efaab
safalnamdeo <safalnamdeo@gmail.com>
2025-05-20 11:13:20 +0530

    Add files via upload

---
+++ r2.java
@@ -0,0 +1,16 @@
+public class r2 {
+    r2()
+    {
+        System.out.println("ee sala cup namde");
+    }
+    void eat()
+    {
+        System.out.println("ipl 2025");
+    }
+}
+class hello{
+    public static void main(String[] args) {
+        r2 obj=new r2();
+        obj.eat();
+    }
+}
