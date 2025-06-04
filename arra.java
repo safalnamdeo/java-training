9b2a7ad7d61ad62431bafbad217a92a45f6e89b3
safalnamdeo <safalnamdeo@gmail.com>
2025-06-04 10:59:41 +0530

    Add files via upload

---
+++ arra.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class arra {
+    public static void main(String[] args) {
+        int[] arr1={2,-7,0,5,6,-3,0,1};
+
+        ArrayList<String> arr=new ArrayList<>();
+        arr.add("a");
+        arr.add("B");
+        arr.add("C");
+        Collections.sort(arr);
+        System.out.println(arr);
+    }
+}
