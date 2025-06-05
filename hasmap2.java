19518aeb81af468135747084bae620eecdad0592
safalnamdeo <safalnamdeo@gmail.com>
2025-06-05 12:56:08 +0530

    Add files via upload

---
+++ hasmap2.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class hasmap2{
+
+
+    public static void main(String[] args) {
+        HashMap<Integer , Integer> map= new HashMap<>();
+        map.put(1, 1);
+        map.put(2,2);
+        map.put(3,5);
+       map.forEach((key,value) -> {
+            System.out.println(key +  "  "+ value);
+        });
+    }
+}
+
