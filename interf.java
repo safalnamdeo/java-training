32e95bd139230ed1e67b3f5a1409a03196247693
safalnamdeo <safalnamdeo@gmail.com>
2025-06-10 10:44:20 +0530

    Add files via upload

---
+++ interf.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+
+/*interface Client {
+    void print();
+    void show();
+
+}
+class interf extends {
+    public void print(){
+        System.out.println("cccc");
+    }
+    public void show()
+    {
+
+    }
+}
+class hello{
+    public static void main(String[]args){
+        int obj
+    }
+}*/
