4e21ea730d9f30d341fe5cede850391dd3aef017
safalnamdeo <safalnamdeo@gmail.com>
2025-06-11 11:00:28 +0530

    Add files via upload

---
+++ bank.java
@@ -0,0 +1,16 @@
+/*import java.util.ArrayList;
+
+class Account {
+    private String name;
+    private int accountNumber;
+    private double balance;
+    public Account{String name,int accountNumber,double balance};
+    this.accountNumber=accountNumber;
+    this.name=name;
+    this.balance=balance;
+
+
+}
+ArrayList<Transaction> transactions=new ArrayList<>()
+
+ */
